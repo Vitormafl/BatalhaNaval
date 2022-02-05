c0d3d1cd28b8d20612099dd12d9b8c2423d806fe
Vitormafl <97552556+Vitormafl@users.noreply.github.com>
2022-02-05 13:12:27 -0300

    Create api.h

---
+++ api.h
@@ -0,0 +1,41 @@
+#ifndef api_h
+#define api_h
+
+// Coordenada player
+	typedef struct _coord{
+	
+		struct _coord *n;
+		struct _coord *s;
+		struct _coord *e;
+		struct _coord *w;
+		char simb;
+		char type;
+		int ori;	//0 = Horizontal | 1 = Vertical
+	
+	} coord;
+
+// Coordenada bot
+	typedef struct _coordb{
+	
+		struct _coordb *n;
+		struct _coordb *s;
+		struct _coordb *e;
+		struct _coordb *w;
+		char simb;
+		char simb_ex;
+		char type;
+		int ori; 	//0 = Horizontal | 1 = Vertical
+	
+	} coordB;
+
+void iniciarTabuleiro(coord**, coordB**);
+
+void printarTabuleiro(coord*, coordB*);
+
+void PreencherTabuleiro(coord** tab);
+
+int input(coordB** );
+
+void reset(coord* , coordB* );
+
+#endif
