5ad574d9821388c6fd8f2c36a9d556438d257039
nicolasduartesantos <83790896+nicolasduartesantos@users.noreply.github.com>
2022-02-05 17:52:09 -0300

    Update player.c

--- player.c
+++ player.c
@@ -2,44 +2,47 @@
 #include <stdlib.h>
 #include "api.h"
 
-int input(coord* tabP, coord* tabB, int* pontP, int* pontB){
-
-    char input[5]; char coordenadas[2];
-
-    scanf("%s", &input);
-
-    for (int i = 0; i < 5; i++)
+int input(coord* tabP, coord* tabB, int* pontP, int* pontB)
+{
+    while (true)
     {
-        if (isalpha(input[i]))
+        char input[5]; char coordenadas[2];
+
+        scanf("%s", &input);
+
+        for (int i = 0; i < 5; i++)
         {
-            if ((int)(input[i]) >= 77) return 0; 
-            coordenadas[0] = input[i];
+            if (isalpha(input[i]))
+            {
+                if (int(input[i]) >= 77) continue; 
+                coordenadas[0] = input[i];
+            }
+
+            if (isdigit(input[i])) 
+            {
+                if (input[i] < 1 or input[i] > 12) continue; 
+                coordenadas[1] = input[i];
+            }
         }
 
-        if (isdigit(input[i])) 
+        for (int i = 0; i < int(coordenadas[0]); i++)
         {
-            if (input[i] < 1 or input[i] > 12) return 0; 
-            coordenadas[1] = input[i];
+            noh = noh->e;
         }
+
+        for (int i = 0; i < coordenadas[1]; i++)
+        {
+            noh = noh->s;
+        }
+
+        if (noh->type != '0')
+        {
+            noh->simb = '*';
+            verificarJogada(tabP, tabB, noh, NULL, pontP, pontB);
+        }
+
+        else noh->simb = 'O';
+
+        break;
     }
-
-    for (int i = 0; i < (int)(coordenadas[0]); i++)
-    {
-        noh = noh->e;
-    }
-
-    for (int i = 0; i < coordenadas[1]; i++)
-    {
-        noh = noh->s;
-    }
-
-    if (noh->type != '0')
-    {
-        noh->simb = '*';
-        verificarJogada(tabP, tabB, noh, NULL, pontP, pontB);
-    }
-
-    else noh->simb = 'O';
-
-    return 1;
 }
