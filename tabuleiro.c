7aed946c005be65157f94952637de14dfdc95d44
Guilherme de Menezes Furtado <63206585+Walrusbane524@users.noreply.github.com>
2022-02-05 19:06:39 -0300

    Update tabuleiro.c

--- tabuleiro.c
+++ tabuleiro.c
@@ -641,12 +641,262 @@ void verificarJogada(coord* tabP, coord* tabB, coord* acertoP, coord* acertoB, i
 	}
 }
 
+int input(coord* tabP, coord* tabB, int* pontP, int* pontB){
+
+    char input[5]; char coordenadas[2];
+    coord* noh = tabB;
+
+    scanf("%s", &input);
+
+    for (int i = 0; i < 5; i++)
+    {
+        if (isalpha(input[i]))
+        {
+            if ((int)(input[i]) >= 77) return 0; 
+            coordenadas[0] = input[i];
+        }
+
+        if (isdigit(input[i])) 
+        {
+            if (input[i] < 1 || input[i] > 12) return 0; 
+            coordenadas[1] = input[i];
+        }
+    }
+
+    for (int i = 0; i < (int)(coordenadas[0]); i++)
+    {
+        noh = noh->e;
+    }
+
+    for (int i = 0; i < coordenadas[1]; i++)
+    {
+        noh = noh->s;
+    }
+
+    if (noh->type != '0')
+    {
+        noh->simb_ex = '*';
+        verificarJogada(tabP, tabB, noh, NULL, pontP, pontB);
+    }
+
+    else noh->simb_ex = 'O';
+
+    return 1;
+}
+
+void jogada(coord* tabPlayer, coord* tabBot, int ver, coord** ant, int* pontP, int* pontB){
+
+	coord *aux;
+	
+	int l, c, escolhendo = 1, escolha;
+	
+	if (ver == 0){
+		while(escolhendo){
+		
+			srand(time(NULL));
+			l = rand()%12;
+			c = rand()%12;
+			
+			aux = tabPlayer;
+			escolhendo = 0;
+			
+			for(int i = 0; i <= l; i++){
+				
+				aux = aux->s;
+				for(int j = 0; j <= c; j++)
+					aux = aux->e;
+			}
+		
+			if(aux->simb == '*' || aux->simb == 'O')
+				escolhendo = 1;
+			else if(aux->simb == ' '){
+				aux->simb = 'O';
+				return;
+			}
+			else{
+				aux->simb = '*';
+				*ant = aux;
+				return verificarJogada(tabPlayer, tabBot, NULL, *ant, pontP, pontB);
+			}
+		}
+	}
+	
+	else{
+		aux = *ant;
+		
+		//Verifica para a direita
+		if(aux->e->simb == '*'){
+		
+			while(aux->e != NULL && aux->e->simb == '*')
+				aux = aux->e;
+				
+			if (aux->e == NULL)
+				aux = *ant;
+				
+			else if(aux->e->simb == 'O')
+				aux = *ant;
+		
+			else if(aux->e->simb == ' '){
+				aux->e->simb = 'O';
+				return;
+			}
+			else{
+				aux->e->simb = '*';
+				*ant = aux->e;
+			
+				return verificarJogada(tabPlayer, tabBot, NULL, *ant, pontP, pontB);
+			}
+		}
+		
+		//Verifica para a esquerda
+		else if(aux->w->simb == '*'){
+		
+			while(aux->w != NULL && aux->w->simb == '*')
+				aux = aux->w;
+				
+			if (aux->w == NULL)
+				aux = *ant;
+					
+			else if(aux->w->simb == 'O')
+				aux = *ant;
+			
+				else if(aux->w->simb == ' '){
+				aux->w->simb = 'O';
+				return;
+			}
+			else{
+				aux->w->simb = '*';
+				*ant = aux->w;
+				
+				return verificarJogada(tabPlayer, tabBot, NULL, *ant, pontP, pontB);
+			}
+		}
+		
+			
+		//Verifica para cima
+		else if(aux->n->simb == '*'){
+			while(aux->n != NULL && aux->n->simb == '*')
+				aux = aux->n;
+			
+			if (aux->n == NULL)
+				aux = *ant;
+					
+			else if(aux->n->simb == 'O')
+				aux = *ant;
+			
+			else if(aux->n->simb == ' '){
+				aux->n->simb = 'O';
+				return;
+			}
+			else{
+				aux->n->simb = '*';
+					*ant = aux->n;
+				return verificarJogada(tabPlayer, tabBot, NULL, *ant, pontP, pontB);
+			}
+		}
+		
+		//Verifica para baixo
+		else if(aux->s->simb == '*'){
+		
+			while(aux->s != NULL && aux->s->simb == '*')
+				aux = aux->s;
+			
+			if (aux->s == NULL)
+				aux = *ant;
+					
+			else if(aux->s->simb == 'O')
+				aux = *ant;
+			
+			else if(aux->s->simb == ' '){
+				aux->s->simb = 'O';
+				return;
+			}
+			else{
+				aux->s->simb = '*';
+				*ant = aux->s;
+				return verificarJogada(tabPlayer, tabBot, NULL, *ant, pontP, pontB);
+			}
+		}
+		
+		//Escolhe aletóriamente um dos lados da coordenada atingida.
+		else{
+			
+			while(escolhendo){
+				aux = *ant;
+				srand(time(NULL));
+				escolha = rand()%4;
+				escolhendo = 0;
+			
+				if(escolha == 0)
+					aux = aux->e;
+				else if(escolha == 1)
+					aux = aux->w;
+				else if(escolha == 2)
+					aux = aux->n;
+				else if(escolha == 3)
+					aux = aux->s;
+					
+				if(aux == NULL)
+					escolhendo = 1;
+				else{
+					if(aux->simb == '*' || aux->simb == 'O')
+						escolhendo = 1;
+					else if(aux->simb == ' '){
+						aux->simb = 'O';
+						return;
+					}
+					else{
+						aux->simb = '*';
+						*ant = aux;
+						return verificarJogada(tabPlayer, tabBot, NULL, *ant, pontP, pontB);
+				
+					}
+				}
+			}
+		}
+		
+		//Escolhe aleatóriamente qualquer coordenada caso alguma desgraça acometa os if anteriores.
+		while(escolhendo){
+		
+			srand(time(NULL));
+			l = rand()%12;
+			c = rand()%12;
+			
+			aux = tabPlayer;
+			escolhendo = 0;
+			
+			for(int i = 0; i <= l; i++){
+				aux = aux->s;
+				
+				for(int j = 0; j <= c; j++)
+					aux = aux->e;
+			}
+		
+			if(aux->simb == '*' || aux->simb == 'O')
+				escolhendo = 1;
+			else if(aux->simb == ' '){
+				aux->simb = 'O';
+				return;
+			}
+			else{
+				aux->simb = '*';
+				*ant = aux;
+				verificarJogada(tabPlayer, tabBot, NULL, *ant, pontP, pontB);
+			}
+		}
+	}
+}
+
 //Para testes
 int main(){
 	
+	int pontP = 0, pontB = 0;
 	coord *tabP = NULL; 
-	coordB *tabB = NULL;
+	coord *tabB = NULL;
 	iniciarTabuleiro(&tabP, &tabB);
+	PreencherTabuleiro(tabP);
+	PreencherTabuleiro(tabB);
+	printarTabuleiro(tabP, tabB);
+	input(tabP, tabB, &pontP, &pontB);
 	printarTabuleiro(tabP, tabB);
 }
-
