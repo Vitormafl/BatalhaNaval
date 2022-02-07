4df8eb24255df6a8e89e065a5621aeca05697556
nicolasduartesantos <83790896+nicolasduartesantos@users.noreply.github.com>
2022-02-07 13:53:19 -0300

    Update player.c

--- player.c
+++ player.c
@@ -3,20 +3,25 @@
 #include <ctype.h>
 #include "api.h"
 
-int input(coord* tabP, coord* tabB, int* pontP, int* pontB, int* ver)
+//função da jogada do player
+void input(coord* tabP, coord* tabB, int* pontP, int* pontB, int* ver)
 {
 
     coord* noh = tabB;
     int cont = 1;
 
+    //o loop while só é quebrado quando uma jogada válida é recebida
     while(1)
     {
+        //vetor input tem um limite arbitrário de 5 caracteres recebidos
         char input[5]; int coordenadas[2];
 
         scanf("%s", input);
 	
+        //loop for avalia cada um dos caracteres inseridos em input
         for (int i = 0; i < 5; i++)
         {
+            //caso em que input[i] é uma letra
             if (isalpha(input[i]))
             {
 		if ((int)(input[i]) > 96)
@@ -24,6 +29,7 @@ int input(coord* tabP, coord* tabB, int* pontP, int* pontB, int* ver)
 		else
 			coordenadas[0] = input[i] - 64;
             }
+            //caso em que input[i] é um dígito
             if (isdigit(input[i])){
             	if(cont == 1){
                 	coordenadas[1] = input[i] - 48;
@@ -35,21 +41,24 @@ int input(coord* tabP, coord* tabB, int* pontP, int* pontB, int* ver)
                 cont++;
             }
         }
+
+        //casos inválidos
         if(coordenadas[0] > 12) continue;
         if(coordenadas[1] > 12) continue;
-        
-	printf("%d, %d\n", coordenadas[0], coordenadas[1]);
 	
+        //movimentação até o nó na coordenada horizontal
         for (int c = 1; c < coordenadas[0]; c++)
         {
             noh = noh->e;
         }
 
+        //movimentação até o nó na coordenada vertical
         for (int l = 1; l < coordenadas[1]; l++)
         {
             noh = noh->s;
         }
-	
+
+        //caso em que acertou um navio
         if (noh->type != '0')
         {
 
