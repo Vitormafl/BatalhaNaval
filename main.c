f92bc91e25efc41c63298ee0ef02bfef171bdba6
Guilherme de Menezes Furtado <63206585+Walrusbane524@users.noreply.github.com>
2022-02-07 21:15:32 -0300

    Update main.c

--- main.c
+++ main.c
@@ -41,7 +41,7 @@ int main(){
 	int iniciar_jogo = 1;
 	int pontP = 0, pontB = 0, ver = 0, pontBG = 0, pontPG = 0;
 	coord* ant = NULL;
-	coord *tabP = NULL; 
+	coord *tabP = NULL;
 	coord *tabB = NULL;
 	iniciarTabuleiro(&tabP, &tabB);
 	
@@ -56,15 +56,14 @@ int main(){
 		while(pontB < 8 && pontP < 8){
 			input(tabP, tabB, &pontP, &pontB, &ver);
 			jogadaBot(tabP, tabB, &ver, &ant, &pontP, &pontB);
-			pontP++;
 		}
-		if(pontP = 8){
+		if(pontP == 8){
 			printf("\nParabéns, você derrotou a melhor máquina de batalha naval!\n");
 			pontPG++;
 		}
 		else{
-			printf("Você foi derrotado\n");
-			pontBG++;	
+			printf("Você foi derrotado.\n");
+			pontBG++;
 		}
 		
 		printf("\nJogos ganhos:\n\nPlayer: %d      Computador: %d\n", pontPG, pontBG);
@@ -73,4 +72,3 @@ int main(){
 		if(iniciar_jogo == 1) reset(tabP, tabB, &pontP, &pontP);
 	}
 }
-
